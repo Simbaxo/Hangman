872f66b445381611b827b4a19d8919b13946ea18
Andres Mena <ballerlikemike23@gmail.com>
2018-08-20 14:24:55 -0400

    Manual Guess Logic

--- hangman.js
+++ hangman.js
@@ -1,7 +1,7 @@
 const Hangman = function (word, remainingGuesses) {
   this.word = word.toLowerCase().split('')
   this.remainingGuesses = remainingGuesses
-  this.guessedLetters = ['c', 'e']
+  this.guessedLetters = []
 }
 
 Hangman.prototype.getPuzzle = function () {
@@ -18,8 +18,27 @@ Hangman.prototype.getPuzzle = function () {
   return puzzle
 }
 
+Hangman.prototype.makeGuess = function (guess) {
+  guess = guess.toLowerCase()
+  const isUnique = !this.guessedLetters.includes(guess)
+  const isBadGuess = !this.word.includes(guess)
+
+  if (isUnique) {
+    this.guessedLetters.push(guess)
+  }
+
+  if (isUnique && isBadGuess ) {
+    this.remainingGuesses--
+  }
+}
+
 const game1 = new Hangman('Cat', 2)
+game1.makeGuess('c')
+game1.makeGuess('t')
+game1.makeGuess('z')
 console.log(game1.getPuzzle())
+console.log(game1.remainingGuesses)
 
 const game2 = new Hangman('New Jersey', 4)
+game2.makeGuess('w')
 console.log(game2.getPuzzle())
