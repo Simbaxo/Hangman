75bf735feee16892825d2aa2d1b46f4d986e8e4d
Andres Mena <ballerlikemike23@gmail.com>
2018-08-20 14:39:45 -0400

    Config App.js

--- hangman.js
+++ hangman.js
@@ -31,19 +31,3 @@ Hangman.prototype.makeGuess = function (guess) {
     this.remainingGuesses--
   }
 }
-
-const puzzleEl = document.querySelector('#puzzle')
-const guessesEl = document.querySelector('#guesses')
-const game1 = new Hangman('Cat', 2)
-
-puzzleEl.textContent = game1.getPuzzle()
-guessesEl.textContent = game1.remainingGuesses
-console.log(game1.getPuzzle())
-console.log(game1.remainingGuesses)
-
-window.addEventListener('keypress', function (e) {
-  const guess = String.fromCharCode(e.charCode)
-  game1.makeGuess(guess)
-  puzzleEl.textContent = game1.getPuzzle()
-  guessesEl.textContent = game1.remainingGuesses
-})
