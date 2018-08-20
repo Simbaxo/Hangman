9fb142e3f07195624e2f3b02a208dbb65ae27548
Andres Mena <ballerlikemike23@gmail.com>
2018-08-20 13:06:58 -0400

    Primitives and Objects Part 1

---
+++ app.js
@@ -0,0 +1,10 @@
+// product --> Object.prototype --> null
+const product = new Object({
+  name: 'The War of Art'
+})
+
+Object.prototype.someNewMethod = () => `This is the new function`
+
+// hasOwnProperty
+console.log(product.someNewMethod())
+console.log(product)
