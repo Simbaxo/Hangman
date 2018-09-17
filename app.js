1d4a541a1c523398b9f69b8b82be84560c1ed464
Andres Mena <ballerlikemike23@gmail.com>
2018-09-17 19:12:06 -0400

    Country Callback Abstraction Challenge

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ window.addEventListener('keypress', (e) => {
   guessesEl.textContent = game1.statusMessage
 })
 
-getPuzzle((error, puzzle) => {
+getPuzzle("2", (error, puzzle) => {
   if (error) {
     console.log(`Error: ${error}`)
   } else {
@@ -26,19 +26,11 @@ getPuzzle((error, puzzle) => {
 
 // Making an HTTP request
 
-// const countryCode = "US"
-// const countyRequest = new XMLHttpRequest
-
-// countyRequest.addEventListener('readystatechange', (e) => {
-//   if (e.target.readyState === 4 && e.target.status === 200) {
-//     const data = JSON.parse(e.target.responseText)
-//     const country = data.find((country) => country.alpha2Code === countryCode)
-//     console.log(country.name)
-//   } else if (e.target.readyState === 4) {
-//     console.log('Unable to fetch data')
-//   }
-// })
-
-// countyRequest.open('GET', 'https://restcountries.eu/rest/v2/all')
-// countyRequest.send()
+getCountry('US', (error, country) => {
+  if (error) {
+    console.log(`Error: ${error}`)
+  } else {
+    console.log(`Country name: ${country.name}`)
+  }
+})
 
--- requests.js
+++ requests.js
@@ -1,4 +1,4 @@
-const getPuzzle = (callback) => {
+const getPuzzle = (wordCount, callback) => {
   const request = new XMLHttpRequest()
 
   request.addEventListener('readystatechange', (e) => {
@@ -10,6 +10,23 @@ const getPuzzle = (callback) => {
     }
   })
 
-  request.open('GET', 'http://puzzle.mead.io/puzzle')
+  request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
   request.send()
+}
+
+const getCountry = (countryCode, callback) => {
+  const countyRequest = new XMLHttpRequest
+
+  countyRequest.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+      const data = JSON.parse(e.target.responseText)
+      const country = data.find((country) => country.alpha2Code === countryCode)
+      callback(undefined, country)
+    } else if (e.target.readyState === 4) {
+      callback('Unable to fetch data', undefined)
+    }
+  })
+
+  countyRequest.open('GET', 'https://restcountries.eu/rest/v2/all')
+  countyRequest.send()
 }
