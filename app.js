408409be1149ba54fff7847d1cc202a6612d9bce
Andres Mena <ballerlikemike23@gmail.com>
2018-09-17 18:54:45 -0400

    Callback Abstraction

--- app.js
+++ app.js
@@ -16,37 +16,29 @@ window.addEventListener('keypress', (e) => {
   guessesEl.textContent = game1.statusMessage
 })
 
+getPuzzle((error, puzzle) => {
+  if (error) {
+    console.log(`Error: ${error}`)
+  } else {
+    console.log(puzzle)
+  }
+})
+
 // Making an HTTP request
-const request = new XMLHttpRequest()
 
-request.addEventListener('readystatechange', (e) => {
-  if (e.target.readyState === 4 && e.target.status === 200) {
-    const data = JSON.parse(e.target.responseText)
-    console.log(data)
-  } else if (e.target.readyState === 4) {
-    console.log('An error has taken place')
-  }
-})
+// const countryCode = "US"
+// const countyRequest = new XMLHttpRequest
 
-request.open('GET', 'http://puzzle.mead.io/puzzle')
-request.send()
+// countyRequest.addEventListener('readystatechange', (e) => {
+//   if (e.target.readyState === 4 && e.target.status === 200) {
+//     const data = JSON.parse(e.target.responseText)
+//     const country = data.find((country) => country.alpha2Code === countryCode)
+//     console.log(country.name)
+//   } else if (e.target.readyState === 4) {
+//     console.log('Unable to fetch data')
+//   }
+// })
 
-// 1. Make a new request for all the countries
-// 2. Parse the responseText to get back the array of objects
-// 3. Find your country object by it's country code
-// 4. Print the full country name
-
-const countryCode = "US"
-const countyRequest = new XMLHttpRequest
-
-countyRequest.addEventListener('readystatechange', (e) => {
-  if (e.target.readyState === 4 && e.target.status === 200) {
-    const data = JSON.parse(e.target.responseText)
-    const country = data.find((country) => country.alpha2Code === countryCode)
-    console.log(country.name)
-  }
-})
-
-countyRequest.open('GET', 'https://restcountries.eu/rest/v2/all')
-countyRequest.send()
+// countyRequest.open('GET', 'https://restcountries.eu/rest/v2/all')
+// countyRequest.send()
 
